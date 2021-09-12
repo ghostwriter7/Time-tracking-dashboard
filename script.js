a84aa7fa4b6431e76253b4ba0653e811897a6df1
ghostwriter7 <87965894+ghostwriter7@users.noreply.github.com>
2021-09-12 09:44:21 +0300

    Fix bug
    The labels in the panels (Last Week - 35hrs) were static. They are generated dynamically now.

--- script.js
+++ script.js
@@ -21,6 +21,12 @@ function updateUI(array, frequency) {
     //construct ID for searching for elements
     const currentType = type.title.toLowerCase().replace(" ", "");
 
+    //create new label (Last Day/Week/Month)
+    const label =
+      (frequency === "daily" && "Day") ||
+      (frequency === "weekly" && "Week") ||
+      (frequency === "monthly" && "Month");
+
     //get 'big-number' element and update its content
     document.getElementById(
       currentType
@@ -29,6 +35,6 @@ function updateUI(array, frequency) {
     //get previous element and update its content
     document.getElementById(
       `prev${currentType}`
-    ).innerText = `Last Week - ${type.timeframes[frequency].previous}hrs`;
+    ).innerText = `Last ${label} - ${type.timeframes[frequency].previous}hrs`;
   });
 }
